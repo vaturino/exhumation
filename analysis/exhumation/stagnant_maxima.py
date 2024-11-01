6b7646ef238d0a737e72017e51750f42ce8bcc8b
vaturino <valeria.turino2@gmail.com>
2024-11-01 09:41:42 -0400

    mods to stagnant

--- analysis/exhumation/stagnant_maxima.py
+++ analysis/exhumation/stagnant_maxima.py
@@ -29,6 +29,52 @@ from libraries.particles import *
 from libraries.exhumation import *  
 import plotly.express as px
 
+
+def load_data(id, txt_loc):
+    data = pd.read_csv(f"{txt_loc}/PT/pt_part_{id}.txt", sep="\s+")
+    data["Plith"] = (901.e3 - data["y"])*3100*9.81/1e9
+    data["time"] = data["time"]/2
+    data["ts"] = data["time"]*2
+    data["T"] = (data["T"] + 0.6*(data["depth"].max() - data["depth"])) - 273.
+    return data
+
+def compute_stagnant_maxima(data, Pthresh):
+    data["Pdiff"] = data.groupby("time_bin")["Plith"].transform(lambda x: abs(x.max() - x.min()))
+    data.loc[data["time"] <= 10., "Pdiff"] = np.nan
+    data["Pmaxloc"] = data.apply(lambda row: data[(data["time_bin"] == row["time_bin"]) & (data["Pdiff"] <= Pthresh)]["Plith"].median() if pd.notna(row["Pdiff"]) and row["Pdiff"] <= Pthresh else np.nan, axis=1)
+
+    # Step 1: Mark non-NaN Pmaxloc values
+    data['Pmaxloc_not_nan'] = data['Pmaxloc'].notna()
+
+    # Step 2: Create time_bin_consecutive
+    data['time_bin_consecutive'] = np.where(
+        data['Pmaxloc_not_nan'],
+        (data['Pmaxloc_not_nan'] & (data['time_bin'] != data['time_bin'].shift())).cumsum(),
+        np.nan
+    )
+    #if time_bin_consecutive is numeric and goes from value to value+1, use value instead of value+1
+    for i in range(0, len(data)-1):
+        if data["time_bin_consecutive"].iloc[i+1] > data["time_bin_consecutive"].iloc[i]:
+            if abs(data["Pmaxloc"].iloc[i] - data["Pmaxloc"].iloc[i+1]) < Pthresh:
+                data["time_bin_consecutive"].iloc[i+1] = data["time_bin_consecutive"].iloc[i]
+    
+    # see if the particle is rising, subducting or truly stagnant
+    lastPdata = data["Plith"].iloc[-1] - data["Plith"].iloc[-10]
+    if lastPdata <= - data["Plith"].max()/100.: 
+        data["direction"] = "rising"
+    elif lastPdata > data["Plith"].max()/100.:
+        data["direction"] = "subducting"
+    else:
+        data["direction"] = "stagnant"
+    
+    # calculate difference in Plith between first value of one time_bin_consecutive_new and the last value of the next time_bin_consecutive_new
+    data['Pdiff_new'] = data.groupby('time_bin_consecutive')['Plith'].transform(lambda x: x.iloc[-1] - x.iloc[0])
+    data['Pmaxloc_new'] = data.groupby('time_bin_consecutive')['Pmaxloc'].transform("mean")
+    return data
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(description= 'Script that gets some models and gives the kinematic indicators')
     parser.add_argument('json_file', help='json file with model name, time at the end of computation, folder where to save the plots, legend')
@@ -53,68 +99,45 @@ def main():
         os.mkdir(sloc)
 
     part = pd.read_csv(f"{txt_loc}/stagnant_particles.txt", sep="\s+")
-
-    for p in tqdm(0, range(len(part)), 10):
-        id = part[p]
-        data = pd.read_csv(f"{txt_loc}/PT/pt_part_{id}.txt", sep="\s+")
-        data["Plith"] = (901.e3 - data["y"])*3100*9.81/1e9
-        data["time"] = data["time"]/2
-        data["ts"] = data["time"]*2
+    particles = pd.DataFrame(columns=["id", "lithology",  "direction", "timePmax", "Pmax", "Tmax", "timeP1", "P1", "T1", "timeP2", "P2", "T2", "timeP3", "P3", "T3", "timeP4", "P4", "T4",], index=range(len(part)))
 
 
-        tin = 0
-        for i in range(len(data)):
-            if (data.y.iloc[0] - data.y.iloc[i]) > 2.:
-                tin = data.time.iloc[i]
-                break
+    for p in tqdm(range(0, len(part))):
+        id = part["id"].iloc[p]
+        data = load_data(id, txt_loc)
+
+        Pthresh = data["Plith"].max()/20
 
         # Bin data by time intervals of 10 Myr
-        bins = np.arange(0, 51, 10)
+        bins = np.arange(0, 51, 5)
         data["time_bin"] = pd.cut(data["time"], bins)
 
-        data["Pdiff"] = data.groupby("time_bin")["Plith"].transform(lambda x: abs(x.max() - x.min()))
-        data.loc[data["time"] <= tin, "Pdiff"] = np.nan
-        data[data["Pdiff"] <= data["Plith"].max()/10]
-        data["Pmaxloc"] = data.apply(lambda row: data[(data["time_bin"] == row["time_bin"]) & (data["Pdiff"] <= 0.15)]["Plith"].median() if pd.notna(row["Pdiff"]) and row["Pdiff"] <= 0.15 else np.nan, axis=1)
-
-        # Step 1: Mark non-NaN Pmaxloc values
-        data['Pmaxloc_not_nan'] = data['Pmaxloc'].notna()
-
-        # Step 2: Create time_bin_consecutive
-        data['time_bin_consecutive'] = np.where(
-            data['Pmaxloc_not_nan'],
-            (data['Pmaxloc_not_nan'] & (data['time_bin'] != data['time_bin'].shift())).cumsum(),
-            np.nan
-        )
-
-        #if time_bin_consecutive is numeric and goes from value to value+1, use value instead of value+1
-        for i in range(0, len(data)-1):
-            if data["time_bin_consecutive"].iloc[i+1] > data["time_bin_consecutive"].iloc[i]:
-                if abs(data["Pmaxloc"].iloc[i] - data["Pmaxloc"].iloc[i+1]) < data["Plith"].max()/10:
-                    data["time_bin_consecutive"].iloc[i+1] = data["time_bin_consecutive"].iloc[i]
-        
-        # calculate difference in Plith between first value of one time_bin_consecutive_new and the last value of the next time_bin_consecutive_new
-        data['Pdiff_new'] = data.groupby('time_bin_consecutive')['Plith'].transform(lambda x: x.iloc[-1] - x.iloc[0])
-        data['Pmaxloc_new'] = data.groupby('time_bin_consecutive')['Pmaxloc'].transform("mean")
+        data = compute_stagnant_maxima(data, Pthresh)
 
         #save to particles dataframe
         particles["id"].iloc[p] = id
+        particles["lithology"].iloc[p] = part["lithology"].iloc[p]
+        particles["direction"].iloc[p] = data["direction"].iloc[0]
         particles["timePmax"].iloc[p] = data["time"][data["Plith"].idxmax()]
         particles["Pmax"].iloc[p] = data["Plith"].max()
+        particles["Tmax"].iloc[p] = data["T"][data["Plith"].idxmax()]
 
         for k in range(len(data["Pmaxloc_new"].dropna().unique())): 
             particles[f"P{k+1}"].iloc[p] = data["Pmaxloc_new"].dropna().unique()[k]
             particles[f"timeP{k+1}"].iloc[p] = data[data["time_bin_consecutive"] == data["time_bin_consecutive"].dropna().unique()[k]]["time"].median()
-        
-        plt.plot(data["time"], data["Plith"])
-        plt.plot(data["time"], data["Pmaxloc_new"])
-        plt.savefig(f"{sloc}/particle_{id}.png", dpi=300)
-        plt.close()
+            particles[f"T{k+1}"].iloc[p] = data[data["time_bin_consecutive"] == data["time_bin_consecutive"].dropna().unique()[k]]["T"].mean()
 
+        if p % 10 == 0:
+            plt.plot(data["time"], data["Plith"], color="grey")
+            plt.plot(data["time"], data["Pmaxloc_new"], color="blue")
+            plt.xlabel("Time (Myr)")
+            plt.ylabel("Pressure (GPa)")
+            plt.title(f"Particle {id} - Lithology: {part['lithology'].iloc[p]} - Direction: {data['direction'].iloc[0]}")
+            # plt.ylim(0, 1.8)
+            plt.savefig(f"{sloc}/stagnant_maxima_{id}.png")
+            plt.close()
 
-
-    particles = particles.astype(float)
-    particles["id"] = particles["id"].astype(int)
+    # particles = particles.astype({col: 'float' for col in particles.columns if col != 'lithology' or col != 'direction'})
     particles.to_csv(f"{txt_loc}/stagnant_maxima.txt", sep="\t", index=False, header=True, float_format='%.2f')
 
 
---
+++ comparison/timescale_comparison.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import seaborn as sns
+import json
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as mpatches
+
+
+def main():
+    # Load json file
+    with open('models.json') as f:
+        models = json.load(f)
+
+    plot_loc = "/home/vturino/PhD/projects/exhumation/plots/single_models"
+    tests = ["velocity", "viscosity", "friction", "serpentinization"]
+    names = ["velocity_names", "viscosity_names", "friction_names", "serpentinization_names"]
+
+    # Set colorblind-friendly palette
+    palette = sns.color_palette("colorblind")
+
+    # Create a mapping for lithology colors
+     # Define color palettes
+    colors_tin = {
+        "sed": "midnightblue",
+        "oc": "#733A11",
+        "ecl": "#003300",
+        "serp": "#3b0000"
+    }
+
+    colors_tmax = {
+        "sed": "mediumblue",
+        "oc": "#B06D1A",
+        "ecl": "#45701C",
+        "serp": "brown"
+    }
+
+    colors_tfin = {
+        "sed": "cornflowerblue",
+        "oc": "#E3B64F",
+        "ecl": "#A0C93D",
+        "serp": "lightsalmon"
+    }
+
+
+    # Ensure the output directory exists
+    output_dir = "combined/timing/strip_plots"
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Loop through each test to create a grid plot
+    for idx, test in enumerate(tests):
+        fig, ax = plt.subplots(1, 2, figsize=(20, 8), gridspec_kw={'width_ratios': [1, 1.8]})
+
+        if test in models:
+            model_names = models[names[tests.index(test)]]
+            all_data_exhumed = []  # List to store all dataframes for exhumed particles
+            all_data_stagnant = []  # List to store all dataframes for stagnant particles
+
+            for ind_m, m in enumerate(models[test]):
+                text_loc = f"{plot_loc}/{m}/txt_files"
+                exhumed = pd.read_csv(f"{text_loc}/timing_exhumed_particles.txt", sep="\s+")
+                stagnant = pd.read_csv(f"{text_loc}/timing_stagnant_particles.txt", sep="\s+")
+
+                # Add a new column for the model name from model_names
+                exhumed['model'] = model_names[ind_m]
+                all_data_exhumed.append(exhumed)
+                stagnant['model'] = model_names[ind_m]
+                all_data_stagnant.append(stagnant)
+
+            # Combine all data into a single DataFrame
+            combined_data_exhumed = pd.concat(all_data_exhumed)
+            combined_data_stagnant = pd.concat(all_data_stagnant)
+
+            alpha = 0.2
+            size = 15
+            marker = '$\u25AC$'
+            jitter = False
+            lw = 0
+
+        
+            sns.stripplot(data=combined_data_exhumed, 
+                          x='model', y='tin', 
+                          hue = "lithology", palette = colors_tin, 
+                          ax=ax[0], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size,
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+            sns.stripplot(data=combined_data_exhumed, 
+                          x='model', y='tmax', 
+                          hue = "lithology", palette = colors_tmax, 
+                          ax=ax[0], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size, 
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+            sns.stripplot(data=combined_data_exhumed, 
+                          x='model', y='tfin', 
+                          hue = "lithology", palette = colors_tfin, 
+                          ax=ax[0], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size, 
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+
+            
+
+
+            sns.stripplot(data=combined_data_stagnant, 
+                          x='model', y='tin', 
+                          hue = "lithology", palette = colors_tin, 
+                          ax=ax[1], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size, 
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+            sns.stripplot(data=combined_data_stagnant, 
+                          x='model', y='tmax', 
+                          hue = "lithology", palette = colors_tmax, 
+                          ax=ax[1], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size, 
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+            sns.stripplot(data=combined_data_stagnant, 
+                          x='model', y='tfin', 
+                          hue = "lithology", palette = colors_tfin, 
+                          ax=ax[1], 
+                          dodge=True, jitter=jitter, 
+                          alpha=alpha, size=size, 
+                          legend=False,
+                          marker= marker,
+                          linewidth=lw)
+
+            ax[0].axhline(y=35, color='k', linestyle='--', linewidth=2)
+            ax[1].axhline(y=35, color='k', linestyle='--', linewidth=2)
+
+            # Adding vertical lines between 'model' categories
+            for i in range(len(combined_data_exhumed['model'].unique()) - 1):
+                ax[0].axvline(x=i + 0.5, color='grey', linestyle='--', linewidth=1)
+                ax[1].axvline(x=i + 0.5, color='grey', linestyle='--', linewidth=1)
+
+            # Calculate the number of unique lithologies
+            num_lithologies = len(combined_data_exhumed['lithology'].unique())
+            
+            # Adjust k proportionally to the number of lithologies
+            k = 0.1 / num_lithologies
+            for i, artist in enumerate(ax[0].collections[1:]):  # Skip the first collection (tin)
+                for path in artist.get_paths():
+                    path.vertices[:, 0] += (i + 1) * k
+            for i, artist in enumerate(ax[1].collections[1:]):
+                for path in artist.get_paths():
+                    path.vertices[:, 0] += (i + 1) * k
+
+            # Load conv_rate data
+            conv_data = pd.read_csv(f"{plot_loc}/{models[test][0]}/txt_files/2D_v.txt", sep="\s+")
+            conv_data["conv_rate"].iloc[0] = np.nan  # Avoid negative diameters
+            conv_data["time"] = conv_data["time"]/1.e6
+
+            # New x position for convergence rate circles (adjusted to the left side)
+            conv_rate_x_position = -0.5  # Positioning slightly left of the violin plots
+
+            # Scatter plot along the y-axis for each subplot
+            for j, axis in enumerate(ax):
+                # Determine y-axis limits for cropping
+                y_limits = axis.get_ylim()
+
+                # Set circle sizes (diameter) proportional to conv_rate
+                size_factor = 100  # Adjust this factor as needed
+                sizes = conv_data["conv_rate"] * size_factor
+
+                # Scatter plot for convergence rate circles on the left side
+                axis.scatter([conv_rate_x_position] * len(conv_data["conv_rate"]), 
+                                conv_data['time'], 
+                                s=sizes, alpha=0.1, color="grey")  # Grey circles for convergence rate
+                
+            
+            # Adding titles and labels for each subplot
+            ax[0].set_title("Exhumed Particles Timing")
+            ax[0].set_xlabel("Model Name")
+            ax[0].set_ylabel("Time (Myr)")
+            ax[0].set_ylim(0, 50)
+
+            ax[1].set_title("Stagnant Particles Timing")
+            ax[1].set_xlabel("Model Name")
+            # ax[1].set_ylabel("Time (Myr)")
+            ax[1].set_ylabel("")
+            ax[1].set_ylim(0, 50)
+
+
+            # # Create the legend for ax[1]
+            # legend1 = ax[1].legend(loc='best', bbox_to_anchor=(1, 1), handletextpad=1.5, labelspacing=1)
+            # legend1.set_title("Lithology")
+
+            # Get handles and labels from ax[1]
+            handles, labels = ax[1].get_legend_handles_labels()
+
+            # Add the legend to ax[0]
+            ax[0].legend(handles, labels, loc='best', bbox_to_anchor=(1, 1), handletextpad=1.5, labelspacing=1, title="Lithology")
+            
+            # Save the plot
+            plt.tight_layout()
+            format = ["png", "svg"]
+            for form in format:
+                plt.savefig(f"{output_dir}/{form}/timing_combined_{test}.{form}", dpi = 500, format = form)
+            # plt.savefig(f"{output_dir}/exhumed_timing_combined_{test}.eps", dpi = 500, format = "eps")
+            plt.close()  # Close the figure after saving
+
+if __name__ == "__main__":
+    main()
